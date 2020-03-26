907b7abe4442a33d661dbd92e5a0c8ce70ad86bb
zolboo1024 <erdenebz@dickinson.edu>
2020-03-26 13:38:47 -0700

    Register done in React

--- src/components/auth/RegisterModal.js
+++ src/components/auth/RegisterModal.js
@@ -1,104 +1,102 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  Form,
-  FormGroup,
-  Label,
-  Input
-} from 'reactstrap';
-import Redux from 'redux';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-//import {register} from '../../actions/authActions';
+import React, { Component } from 'react';
+import axios from 'axios';
+// import DatePicker from 'react-datepicker';
+// import "react-datepicker/dist/react-datepicker.css";
 
-class RegisterModal extends Component {
-  state = {
-    modal: false,
-    name: '',
-    email: '',
-    password: '',
-    msg: null
-  };
+export default class RegisterModal extends Component {
+  constructor(props) {
+    super(props);
 
-  static propTypes = { //Prop types is what it sounds like. Kinda like a
-    //set of premade global variables
-    isAuthenticated:  PropTypes.bool,
-    error: PropTypes.object.isRequired
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      users: []
+    }
   }
-  toggle = () => {
-    this.setState({
-      modal: !this.state.modal //When you toggle, you're switching the state of the modal.
-      //from true to false or vice versa
-    });
-  };
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value});
-  };
-  //When you click submit, the modal should toggle and therefore should disappear
-  onSubmit = e => {
+  componentDidMount() {
+    console.log("Component mounted");
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  onSubmit(e) {
     e.preventDefault();
 
-    this.toggle();
-  };
+    const thisUser = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password,
+    }
+
+    console.log(thisUser);
+
+    axios.post('http://localhost:3000/users/', thisUser)
+      .then(res => console.log(res.data));
+
+    window.password = '/';
+  }
 
   render() {
     return (
-      <div>
-        <h3>
-          Register
-        </h3>
-        {/*For example, Modal component takes as its "input", the Modal variable
-        of the state and whenever it changes, it changes accordingly.
-        */}
-        <Form onSubmit= {this.onSubmit}>
-          <FormGroup>
-            <Label for="name">Name</Label>
-            <Input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name"
-              className="mb-3"
-              onChange={this.onChange}
-            />
-            <Label for="email">Email</Label>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-              className="mb-3"
-              onChange={this.onChange}
-            />
-            <Label for="password">Password</Label>
-            <Input
+    <div>
+      <h3>Create New Space</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}>
+          </input>
+
+        </div>
+        <div className="form-group">
+          <label>Email: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+              />
+        </div>
+        <div className="form-group">
+          <label>Password: </label>
+          <input
               type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-              className="mb-3"
-              onChange={this.onChange}
-            />
-            <Button>
-            <Link to="/" className="nav-link">Register</Link>
-            </Button>
-          </FormGroup>
-        </Form>
-      </div>
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+              />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Create Space Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
     )
   }
 }
-
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated,
-  error: state.error
-});
-
-export default connect(
-  mapStateToProps,
-  {}
-) (RegisterModal);
