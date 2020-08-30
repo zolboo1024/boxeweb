d935206994ab76a7cf487cadb7b5b7a72a9cfd22
zolboo1024 <erdenebz@dickinson.edu>
2020-08-30 18:35:53 -0400

    getting ready for heroku

---
+++ build/precache-manifest.605fa9c5e70826526205ee94714d31bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab246848bd8c838feacb9b818d1b23cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140e34618441252c118d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "140e34618441252c118d",
+    "url": "/static/js/2.03043de1.chunk.js"
+  },
+  {
+    "revision": "d04aaa992181f26b07cd5a56889d2a19",
+    "url": "/static/js/2.03043de1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36cd11b0e89e0058d803",
+    "url": "/static/js/main.186153e8.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
--- server.js
+++ server.js
@@ -80,3 +80,12 @@ app.use('/chat', chatRouter);
 app.listen(port, () => {
   console.log('Server is running on port: ' + port);
 });
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
